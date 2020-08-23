98a8e8904379d47f8577551f1d31234252420062
lzz.io <1219331697@qq.com>
2020-08-23 10:41:06 +0800

    修改apache licenses

--- lzz-core/src/main/java/io/lzz/core/Result.java
+++ lzz-core/src/main/java/io/lzz/core/Result.java
@@ -1,5 +1,5 @@
 /*
- * Copyright [2020] [lzz.io, q1219331697]
+ * Copyright 2020 lzz.io,q1219331697
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
